aa19f84b339be3f0760be5ec55c0ff0ecace4a8c
extendslcc <lucas.lcc@hotmail.com>
2020-05-21 17:43:37 -0300

    :construction: WIP: added github api consumption through axios along with animation style with styled componenets

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from "./styles";
 
 function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [respositories, setRepositories] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setLoading( true );
+        const response = await api.get(`/repos/${newRepo}`)
+
+        console.log(response.data);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        setRepositories([...respositories, data]);
+        setLoading( false );
+
+    }
 
     return (
         <Container>
@@ -12,14 +38,18 @@ function Main() {
                 <FaGithubAlt/>
                 Repositórios
             </h1>
-            <Form onSubmit={() => {
-            }}>
+            <Form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Adicionar respositório!"
+                    onChange={handleInputChange}
                 />
-                <SubmitButton disabled>
-                    <FaPlus color="#FFF" size={14}></FaPlus>
+                <SubmitButton loading={loading}>
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14}/>
+                    ) : (
+                        <FaPlus color="#FFF" size={14}></FaPlus>
+                    )}
                 </SubmitButton>
             </Form>
         </Container>

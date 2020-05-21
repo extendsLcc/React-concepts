dcf927b6576168c07502fb7391df485cc9e41659
extendslcc <lucas.lcc@hotmail.com>
2020-05-21 16:37:12 -0300

    :lipstick: UI: added base layout

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-import { Title } from "./styles";
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, Form, SubmitButton } from "./styles";
 
 function Main() {
 
-    return <Title>Main</Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt/>
+                Repositórios
+            </h1>
+            <Form onSubmit={() => {
+            }}>
+                <input
+                    type="text"
+                    placeholder="Adicionar respositório!"
+                />
+                <SubmitButton disabled>
+                    <FaPlus color="#FFF" size={14}></FaPlus>
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 
 }
 

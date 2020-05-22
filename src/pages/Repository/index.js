7a3d76b561f032f41e45fea650f19e1e95bb3d9e
extendslcc <lucas.lcc@hotmail.com>
2020-05-22 14:15:04 -0300

    :construction: WIP: added request of detail of repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 
-function Repository() {
+import api from "../../services/api";
 
-    return <h1>Respository</h1>;
+function Repository({match}) {
+
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function loadRepositoryData() {
+
+            const repoName = decodeURIComponent(match.params.repository)
+
+            const [repositoryData, issuesData] = await Promise.all([
+                api.get(`repos/${repoName}`),
+                api.get(`repos/${repoName}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            console.log(repositoryData);
+            console.log(issuesData);
+
+            setRepository(repositoryData);
+            setIssues(issuesData);
+            setLoading(false);
+
+        }
+
+        loadRepositoryData();
+
+    }, [])
+
+    return (<h1>Respository: {decodeURIComponent(match.params.repository)}</h1>);
 
 }
+
+Repository.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            repository: PropTypes.string
+        }),
+    }).isRequired
+};
 
 export default Repository;
